4bd28084e2614b8771abf8bb45f4775f322484e8
Jeremy Kings <47997513+jkingsDigiPen@users.noreply.github.com>
2025-07-09 10:46:03 -0700

    Add project for next hero - Gorio, shaman.

---
+++ Gorio/Traits.cs
@@ -0,0 +1,90 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Obeliskial_Content;
+using static Gorio.CustomFunctions;
+using static Gorio.Plugin;
+using UnityEngine;
+
+namespace Gorio
+{
+    [HarmonyPatch]
+    internal class Traits
+    {
+        // list of your trait IDs
+        public static string[] myTraitList = 
+        { 
+        };
+
+        public static void myDoTrait(string _trait, ref Trait __instance)
+        {
+            // get info you may need
+            Enums.EventActivation _theEvent = Traverse.Create(__instance).Field("theEvent").GetValue<Enums.EventActivation>();
+            Character _character = Traverse.Create(__instance).Field("character").GetValue<Character>();
+            Character _target = Traverse.Create(__instance).Field("target").GetValue<Character>();
+            int _auxInt = Traverse.Create(__instance).Field("auxInt").GetValue<int>();
+            string _auxString = Traverse.Create(__instance).Field("auxString").GetValue<string>();
+            CardData _castedCard = Traverse.Create(__instance).Field("castedCard").GetValue<CardData>();
+            Traverse.Create(__instance).Field("character").SetValue(_character);
+            Traverse.Create(__instance).Field("target").SetValue(_target);
+            Traverse.Create(__instance).Field("theEvent").SetValue(_theEvent);
+            Traverse.Create(__instance).Field("auxInt").SetValue(_auxInt);
+            Traverse.Create(__instance).Field("auxString").SetValue(_auxString);
+            Traverse.Create(__instance).Field("castedCard").SetValue(_castedCard);
+            TraitData traitData = Globals.Instance.GetTraitData(_trait);
+            List<CardData> cardDataList = new List<CardData>();
+            List<string> heroHand = MatchManager.Instance.GetHeroHand(_character.HeroIndex);
+            Hero[] teamHero = MatchManager.Instance.GetTeamHero();
+            NPC[] teamNpc = MatchManager.Instance.GetTeamNPC();
+
+            if (!IsLivingHero(_character)) return;
+
+            // activate traits
+            if (_trait == myTraitList[0])
+            {
+                
+            }
+            else if(_trait == myTraitList[1])
+            {
+                
+            }
+            else if(_trait == myTraitList[2])
+            {
+                
+            }
+            else if(_trait == myTraitList[3])
+            {
+                
+            }
+            else if(_trait == myTraitList[4])
+            {
+                
+            }
+            else return;
+
+            LogDebug($"Trait {_trait}");
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Trait), "DoTrait")]
+        public static bool DoTrait(Enums.EventActivation _theEvent, string _trait, Character _character, 
+            Character _target, int _auxInt, string _auxString, CardData _castedCard, ref Trait __instance)
+        {
+            if ((UnityEngine.Object)MatchManager.Instance == (UnityEngine.Object)null)
+                return false;
+            Traverse.Create(__instance).Field("character").SetValue(_character);
+            Traverse.Create(__instance).Field("target").SetValue(_target);
+            Traverse.Create(__instance).Field("theEvent").SetValue(_theEvent);
+            Traverse.Create(__instance).Field("auxInt").SetValue(_auxInt);
+            Traverse.Create(__instance).Field("auxString").SetValue(_auxString);
+            Traverse.Create(__instance).Field("castedCard").SetValue(_castedCard);
+            if (Content.medsCustomTraitsSource.Contains(_trait) && myTraitList.Contains(_trait))
+            {
+                myDoTrait(_trait, ref __instance);
+                return false;
+            }
+            return true;
+        }
+    }
+}
