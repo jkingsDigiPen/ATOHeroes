56947afb57c99702760eff0979f742dcbe499cf1
Jeremy Kings <47997513+jkingsDigiPen@users.noreply.github.com>
2025-07-17 15:01:05 -0700

    Add portraits and set up skins and cardbacks.

--- copyToModmanDir.bat
+++ copyToModmanDir.bat
@@ -1,3 +1,5 @@
 xcopy /i /s /y Corypha\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
 xcopy /i /s /y Gorio\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
 xcopy /i /s /y Maelfas\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
+xcopy /i /s /y Mesmer\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
+xcopy /i /s /y Twilight\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
