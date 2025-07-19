387396feca2d24a5dbdb0361d4a0a72fc2c6f307
Jeremy Kings <47997513+jkingsDigiPen@users.noreply.github.com>
2025-07-18 19:08:20 -0700

    Final (hopefully) subclass name change, add initial hero card and item.

--- copyToModmanDir.bat
+++ copyToModmanDir.bat
@@ -1,5 +1,14 @@
+rmdir /s /q %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx\config\Obeliskial_importing\Corypha
 xcopy /i /s /y Corypha\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
+
+rmdir /s /q %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx\config\Obeliskial_importing\Gorio
 xcopy /i /s /y Gorio\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
+
+rmdir /s /q %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx\config\Obeliskial_importing\Maelfas
 xcopy /i /s /y Maelfas\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
+
+rmdir /s /q %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx\config\Obeliskial_importing\Mesmer
 xcopy /i /s /y Mesmer\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
+
+rmdir /s /q %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx\config\Obeliskial_importing\Twilight
 xcopy /i /s /y Twilight\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
