115dca34d7c58027d4f3529f44db70315699cf77
Jeremy Kings <47997513+jkingsDigiPen@users.noreply.github.com>
2025-07-21 12:15:26 -0700

    Implement trait code for janus.

--- Janus/Traits.cs
+++ Janus/Traits.cs
@@ -15,6 +15,11 @@ namespace Janus
         // list of your trait IDs
         public static string[] myTraitList = 
         {
+            "dualistemissary",
+            "dualistduality",
+            "dualistsacrilege",
+            "dualistavatarlight",
+            "dualistavatarshadow"
         };
 
         public static bool isDamagePreviewActive = false;
@@ -45,29 +50,62 @@ namespace Janus
             if (!IsLivingHero(_character)) return;
 
             // activate traits
-            /*if (_trait == myTraitList[0])
+            if (_trait == myTraitList[0])
             {
-							
+			    // When you apply "Sanctify" charges, heal the most damaged hero for that amount.
+                // When you apply "Dark" charges, apply that amount of "Shield" charges to the most damaged hero.
+                // -These amounts do not gain bonuses-"
+                Character mostDamagedHero = GetLowestHealthCharacter(teamHero);
+                // Just in case!
+                if(mostDamagedHero == null) return;
+
+                if(_auxString == "sanctify")
+                {
+                    TraitHeal(ref _character, mostDamagedHero, _auxInt, _trait);
+                }
+                else if(_auxString == "dark")
+                {
+                    ApplyAuraCurseToTarget("shield", _auxInt, mostDamagedHero, _character, false);
+                }
             }
             else if(_trait == myTraitList[1])
             {
-                
+                // When you play a Healer card, reduce the cost of the highest cost Warrior card in your hand by 1 until discarded.
+                // When you play a Warrior card, reduce the cost of the highest cost Healer card in your hand by 1 until discarded.
+                // (3 times/turn) [4x if you have Avatar of Light]
+                int bonusActivations = _character.HaveTrait(myTraitList[3]) ? 1 : 0;
+                Duality(ref _character,ref _castedCard, Enums.CardClass.Warrior, 
+                    Enums.CardClass.Healer, _trait, bonusActivations : bonusActivations);
             }
             else if(_trait == myTraitList[2])
             {
-                
+                // At the start of your turn, transform all "Bless" charges on monsters to "Sanctify" and "Dark" charges.
+                foreach(NPC monster in teamNpc)
+                {
+                    if(!IsLivingNPC(monster)) continue;
+
+                    int blessStacks = monster.GetAuraCharges("bless");
+                    if(blessStacks < 1) continue;
+
+                    ApplyAuraCurseToTarget("bless", -blessStacks, monster, _character, false);
+                    ApplyAuraCurseToTarget("sanctify", blessStacks, monster, _character, false);
+                    ApplyAuraCurseToTarget("dark", blessStacks, monster, _character, false);
+                }
             }
             else if(_trait == myTraitList[3])
             {
-                
+                // Sanctify +1, Zeal +1, Burn +1. Heal done +30%, heal received +15%. Healer duality can be activated 4 times per turn.
+                // (Handled above)
             }
             else if(_trait == myTraitList[4])
             {
-                
+                // Dark +1, Scourge +1, Chill +1. All damage done +30%. At the start of your turn,
+                // reduce the cost of the "Attack" cards in your hand by 1 until they are discarded.
+                ReduceCardTypeCostUntilDiscarded(Enums.CardType.Attack, 1, ref _character, ref heroHand, ref cardDataList, _trait);
             }
-            else*/ return;
+            else return;
 
-            //LogDebug($"Trait {_trait}");
+            LogDebug($"Trait {_trait}");
         }
 
         [HarmonyPrefix]
