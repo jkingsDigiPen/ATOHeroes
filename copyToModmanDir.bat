0199c514433bbf4ec653d9eabce5a2ef6c31586c
Jeremy Kings <47997513+jkingsDigiPen@users.noreply.github.com>
2025-07-21 08:25:44 -0700

    Update copy batch file.

--- copyToModmanDir.bat
+++ copyToModmanDir.bat
@@ -4,11 +4,11 @@ xcopy /i /s /y Corypha\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossT
 rmdir /s /q %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx\config\Obeliskial_importing\Gorio
 xcopy /i /s /y Gorio\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
 
+rmdir /s /q %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx\config\Obeliskial_importing\Janus
+xcopy /i /s /y Janus\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
+
 rmdir /s /q %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx\config\Obeliskial_importing\Maelfas
 xcopy /i /s /y Maelfas\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
 
 rmdir /s /q %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx\config\Obeliskial_importing\Mesmer
 xcopy /i /s /y Mesmer\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
-
-rmdir /s /q %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx\config\Obeliskial_importing\Twilight
-xcopy /i /s /y Twilight\Thunderstore\BepInEx %AppData%\r2modmanPlus-local\AcrossTheObelisk\profiles\Default\BepInEx
